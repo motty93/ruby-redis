0ff18506cf62da73eef1a25dcc8185d900d9c985
motty93 <rdwbocungelt5@gmail.com>
2024-10-28 22:22:54 +0900

    feat: 証明書を使った接続

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,22 +1,55 @@
+# NOTE: no certificate verification
+# Sidekiq.configure_server do |config|
+#   config.redis = {
+#     url: redis_url,
+#     password: redis_password,
+#     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+#   }
+# end
+#
+# Sidekiq.configure_client do |config|
+#   config.redis = {
+#     url: redis_url,
+#     password: redis_password,
+#     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+#   }
+# end
+
+require 'tempfile'
 require 'sidekiq'
 require 'dotenv/load'
 require_relative '../redis_worker'
 
-redis_url = ENV['REDIS_URL'].gsub('redis://', 'rediss://')
-redis_password = ENV['REDIS_PASSWORD']
+cert_file = Tempfile.new('redis_cert')
+cert_file.write(ENV.fetch('REDIS_CERT', nil))
+cert_file.rewind
+url = ENV.fetch(ENV.fetch('REDIS_PROVIDER', 'REDIS_URL'), nil)&.gsub('redis://', 'rediss://')
+password = ENV.fetch('REDIS_PASSWORD', nil)
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: redis_url,
-    password: redis_password,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    url: url,
+    ssl_params: {
+      cert_store: OpenSSL::X509::Store.new.tap { |store| store.add_file(cert_file.path) },
+      verify_mode: OpenSSL::SSL::VERIFY_PEER,
+    },
+    password: password,
+    size: 20,
+    connect_timeout: 5,
+    reconnect_attempts: 3,
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: redis_url,
-    password: redis_password,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    url: url,
+    ssl_params: {
+      cert_store: OpenSSL::X509::Store.new.tap { |store| store.add_file(cert_file.path) },
+      verify_mode: OpenSSL::SSL::VERIFY_PEER,
+    },
+    password: password,
+    size: 20,
+    connect_timeout: 5,
+    reconnect_attempts: 3,
   }
 end
